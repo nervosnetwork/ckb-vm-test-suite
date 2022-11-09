5a7288499fe941f85657d67a9cff93b605630279
Joii <joii@cryptape.com>
2022-11-09 16:40:12 +0800

    Update dynamically adjust memory

--- binary/src/interpreter32.rs
+++ binary/src/interpreter32.rs
@@ -1,4 +1,4 @@
-use ckb_vm::{run, Bytes, SparseMemory};
+use ckb_vm::{run, Bytes, SparseMemory, RISCV_MAX_MEMORY};
 use std::env;
 use std::process::exit;
 
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let code = std::fs::read(args[0].clone()).unwrap().into();
     let args: Vec<Bytes> = args.into_iter().map(|a| a.into()).collect();
-    let result = run::<u32, SparseMemory<u32>>(&code, &args);
+    let result = run::<u32, SparseMemory<u32>>(&code, &args, RISCV_MAX_MEMORY);
     if result != Ok(0) {
         println!("Error: {:?}", result);
         exit(i32::from(result.unwrap_or(-1)));

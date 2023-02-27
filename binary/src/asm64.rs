859ec8a1660fe0c8008aa36965e06d9bdb8a08b8
mohanson <mohanson@outlook.com>
2023-02-27 21:20:51 +0800

    Add atomic instruction test

--- binary/src/asm64.rs
+++ binary/src/asm64.rs
@@ -1,9 +1,9 @@
 use ckb_vm::{
     machine::{
         asm::{AsmCoreMachine, AsmMachine},
-        DefaultMachineBuilder, VERSION1,
+        DefaultMachineBuilder, VERSION2,
     },
-    Bytes, ISA_IMC,
+    Bytes, ISA_A, ISA_B, ISA_IMC, ISA_MOP,
 };
 use std::env;
 use std::process::exit;
@@ -13,7 +13,11 @@ fn main() {
     let code = std::fs::read(args[0].clone()).unwrap().into();
     let args: Vec<Bytes> = args.into_iter().map(|a| a.into()).collect();
 
-    let asm_core = AsmCoreMachine::new(ISA_IMC, VERSION1, u64::max_value());
+    let asm_core = AsmCoreMachine::new(
+        ISA_IMC | ISA_A | ISA_B | ISA_MOP,
+        VERSION2,
+        u64::max_value(),
+    );
     let core = DefaultMachineBuilder::new(asm_core).build();
     let mut machine = AsmMachine::new(core);
     machine.load_program(&code, &args).unwrap();

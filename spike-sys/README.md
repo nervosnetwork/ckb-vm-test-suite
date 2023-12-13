747b51d981b68f423527309a49d4db6b1379b9c3
mohanson <mohanson@outlook.com>
2023-12-13 10:23:16 +0800

    Update spike-sys's build script

--- spike-sys/README.md
+++ spike-sys/README.md
@@ -3,3 +3,14 @@ A Rust FFI binding for executing instructions in spike (https://github.com/riscv
 ```sh
 $ cargo build
 ```
+
+If the build fails, please install the following dependencies
+
+```sh
+$ sudo apt install device-tree-compiler
+
+$ wget https://apt.llvm.org/llvm.sh
+$ chmod +x llvm.sh
+$ sudo ./llvm.sh 16 all
+$ rm llvm.sh
+```
--- spike-sys/build.sh
+++ spike-sys/build.sh
@@ -1,18 +1,7 @@
 set -ex
 
-if ! command -v clang > /dev/null
-then
-    echo "clang could not be found"
-    exit
-fi
-
-if ! command -v clang++ > /dev/null
-then
-    echo "clang++ could not be found"
-    exit
-fi
-
-if [ ! -d ./riscv-isa-sim ]; then
+if [ ! -f ./target/libspike-interfaces.a ]; then
+    rm -rf riscv-isa-sim
     git clone https://github.com/riscv-software-src/riscv-isa-sim
     cd riscv-isa-sim
     git checkout e6a2245b
@@ -21,14 +10,15 @@ if [ ! -d ./riscv-isa-sim ]; then
     sed -i '/softfloat_install_shared_lib/d' riscv-isa-sim/softfloat/softfloat.mk.in
     sed -i 's/private:/public:/g' riscv-isa-sim/riscv/processor.h
     sed -i 's/private:/public:/g' riscv-isa-sim/riscv/mmu.h
+    sed -i '3 i #include <stdint.h>' riscv-isa-sim/fesvr/device.h
 
     mkdir -p riscv-isa-sim/build
     cd riscv-isa-sim/build
     ../configure CXX=clang++ CC=clang CFLAGS="-g -O1" CXXFLAGS="-g -O1"
     make -j$(nproc)
     cd -
-fi
 
-mkdir -p target
-clang++ -c -fPIC -I./riscv-isa-sim/riscv  -I./riscv-isa-sim/build -I./riscv-isa-sim/softfloat -I./riscv-isa-sim/fesvr src/spike-interfaces.cc -o target/spike-interfaces.o
-ar rcs target/libspike-interfaces.a target/spike-interfaces.o
+    mkdir -p target
+    clang++ -c -fPIC -I./riscv-isa-sim/riscv  -I./riscv-isa-sim/build -I./riscv-isa-sim/softfloat -I./riscv-isa-sim/fesvr src/spike-interfaces.cc -o target/spike-interfaces.o
+    ar rcs target/libspike-interfaces.a target/spike-interfaces.o
+fi

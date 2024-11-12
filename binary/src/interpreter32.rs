e15fa764031c195af7868cf24be80fbd15be4651
mohanson <mohanson@outlook.com>
2024-11-12 15:34:33 +0800

    Update binary

--- binary/src/interpreter32.rs
+++ binary/src/interpreter32.rs
@@ -1,4 +1,8 @@
-use ckb_vm::{run, Bytes, SparseMemory};
+use ckb_vm::{
+    machine::{trace::TraceMachine, DefaultCoreMachine, DefaultMachineBuilder, VERSION2},
+    memory::wxorx::WXorXMemory,
+    Bytes, SparseMemory, ISA_A, ISA_B, ISA_IMC, ISA_MOP,
+};
 use std::env;
 use std::process::exit;
 
@@ -6,7 +10,15 @@ fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let code = std::fs::read(args[0].clone()).unwrap().into();
     let args: Vec<Bytes> = args.into_iter().map(|a| a.into()).collect();
-    let result = run::<u32, SparseMemory<u32>>(&code, &args);
+
+    let core_machine = DefaultCoreMachine::<u32, WXorXMemory<SparseMemory<u32>>>::new(
+        ISA_IMC | ISA_A | ISA_B | ISA_MOP,
+        VERSION2,
+        u64::MAX,
+    );
+    let mut machine = TraceMachine::new(DefaultMachineBuilder::new(core_machine).build());
+    machine.load_program(&code, &args).unwrap();
+    let result = machine.run();
     if result != Ok(0) {
         println!("Error: {:?}", result);
         exit(i32::from(result.unwrap_or(-1)));

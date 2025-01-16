898edc351eeb4de974ca4f0ff8d1e4943a95aecb
Mohanson <mohanson@outlook.com>
2025-01-16 21:37:30 +0800

    Update binary (#24)

--- binary/src/interpreter32.rs
+++ binary/src/interpreter32.rs
@@ -1,7 +1,7 @@
 use ckb_vm::{
     machine::{trace::TraceMachine, DefaultCoreMachine, DefaultMachineBuilder, VERSION2},
     memory::wxorx::WXorXMemory,
-    Bytes, SparseMemory, ISA_A, ISA_B, ISA_IMC, ISA_MOP,
+    SparseMemory, ISA_A, ISA_B, ISA_IMC, ISA_MOP,
 };
 use std::env;
 use std::process::exit;
@@ -9,7 +9,7 @@ use std::process::exit;
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let code = std::fs::read(args[0].clone()).unwrap().into();
-    let args: Vec<Bytes> = args.into_iter().map(|a| a.into()).collect();
+    let args = args.into_iter().map(|a| Ok(a.into()));
 
     let core_machine = DefaultCoreMachine::<u32, WXorXMemory<SparseMemory<u32>>>::new(
         ISA_IMC | ISA_A | ISA_B | ISA_MOP,
@@ -17,7 +17,7 @@ fn main() {
         u64::MAX,
     );
     let mut machine = TraceMachine::new(DefaultMachineBuilder::new(core_machine).build());
-    machine.load_program(&code, &args).unwrap();
+    machine.load_program(&code, args).unwrap();
     let result = machine.run();
     if result != Ok(0) {
         println!("Error: {:?}", result);
